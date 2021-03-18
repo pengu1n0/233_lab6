da20fdae3ab89e74e77d738c53b7a11c1bae5662
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-18 16:08:13 +0700

    Create lab3.c

---
+++ lab3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,j,k,z,a,b,sum,y;
+    char A[2];
+    scanf("%d %d" ,&n,&m);
+    int B[n];
+    for(i=0;i<n;i++)
+    {
+        B[i]=0;
+    }
+    for(i=0;i<m;i++)
+    {
+        scanf("%s" ,A);
+        if(A[0]=='U')
+        {
+            scanf("%d %d" ,&y,&z);
+            B[y]=z;
+        }
+        else if(A[0]=='P')
+        
+}
