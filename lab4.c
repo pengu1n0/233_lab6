1b6bee00201973819d5ad75009d05017dbbbf808
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-18 17:12:06 +0700

    4

--- lab4.c
+++ lab4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int num,max_num=0,min_num=0,i,vote1,vote2,m;
+    int num,max=0,min=0,i,vote1,vote2,m;
     scanf("%d" ,&num);
     int star[num];
     for(i=0;i<num;i++)
@@ -14,4 +14,29 @@ int main()
         star[vote1-1]=star[vote1-1]+1;
         star[vote2-1]=star[vote2-1]+1;
     }
+     max=star[0];
+    min=star[0];
+    for(i=1;i<num;i++)
+    {
+        if(max<star[i])
+        {
+            max=star[i];
+        }
+        if(min>star[i])
+        {
+            min=star[i];
+        }
+    }
+    for(i=0;i<num;i++)
+    {
+        if(max==star[i])
+        {
+            printf("%d " ,i+1);
+        }
+        if(min==star[i])
+        {
+            printf("%d " ,i+1);
+        }
+    }
+    return 0;
 }
